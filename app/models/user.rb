e5b441a0f9bd7d209e7a7601c3d09352df0814a0
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-11-22 23:24:35 +0000

    public/会員登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true,
+                             format: {
+                               with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+                               message: "を全角カタカナのみで入力して下さい。"
+                             }
+  validates :first_name_kana, presence: true,
+                               format: {
+                               with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+                               message: "を全角カタカナのみで入力して下さい。"
+                             }
+
+  validates :email, presence: true
+  
+  validates :telephone_number, presence: true,
+                           format: {
+                             with:   /\A\d{10,11}\z/,
+                             message: "を10桁もしくは11桁の数値で入力して下さい。"
+                           }
+  validates :encrypted_password, presence: true
 end
