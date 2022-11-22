c8e1901015f6476b3a7c368abde8d5deb4e7714d
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-11-22 23:55:27 +0000

    admin/unsubscribe

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
   }
+   namespace :admin do
+    devise_scope :admin do
+      get '/sign_in' => 'sessions#new'
+      post '/sign_in' => 'sessions#create'
+      delete '/sign_out' => 'sessions#destroy'
+    end  
+    get '' => 'homes#top'
+   end  
   
   
   devise_for :users, skip: [:passwords], controllers: {
@@ -10,5 +18,11 @@ Rails.application.routes.draw do
   }
   root to: 'public/homes#top'
   get 'about' => 'public/homes#about', as: '/about'
+  resources :customers, only: [:edit, :update, :show] do
+    collection do
+     get 'unsubscribe'
+     patch 'withdraw'
+    end
+  end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
